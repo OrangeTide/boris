9f28f42a85ed0b592e348d30ae564c9517013192
Jon Mayo <jon.mayo@gmail.com>
2009-01-19 11:00:31 -0800

    moved config parser routines to avoid making prototypes.

--- boris.c
+++ boris.c
@@ -309,12 +309,6 @@ struct telnetclient;
 struct socketio_handle;
 struct menuinfo;
 
-/* TODO: move config routines around so we don't need this */
-struct config_watcher;
-struct config {
-	LIST_HEAD(struct, struct config_watcher) watchers;
-};
-
 struct formitem {
 	LIST_ENTRY(struct formitem) item;
 	char *name;
@@ -362,10 +356,6 @@ static void telnetclient_free(struct socketio_handle *sh);
 EXPORT void menu_show(struct telnetclient *cl, const struct menuinfo *mi);
 EXPORT void menu_input(struct telnetclient *cl, const struct menuinfo *mi, const char *line);
 static void form_menu_lineinput(struct telnetclient *cl, const char *line);
-EXPORT void config_setup(struct config *cfg);
-EXPORT void config_watch(struct config *cfg, const char *mask, int (*func)(struct config *cfg, void *extra, const char *id, const char *value), void *extra);
-EXPORT int config_load(const char *filename, struct config *cfg);
-EXPORT void config_free(struct config *cfg);
 
 /******************************************************************************
  * Util - utility routines
@@ -1635,6 +1625,149 @@ EXPORT void map_test(void) {
 	map_free(&m);
 }
 #endif
+
+/******************************************************************************
+ * Config loader
+ ******************************************************************************/
+struct config_watcher;
+struct config {
+	LIST_HEAD(struct, struct config_watcher) watchers;
+};
+
+struct config_watcher {
+	LIST_ENTRY(struct config_watcher) list;
+	char *mask;
+	int (*func)(struct config *cfg, void *extra, const char *id, const char *value);
+	void *extra;
+};
+
+EXPORT void config_setup(struct config *cfg) {
+	LIST_INIT(&cfg->watchers);
+}
+
+EXPORT void config_free(struct config *cfg) {
+	struct config_watcher *curr;
+	assert(cfg != NULL);
+	while((curr=LIST_TOP(cfg->watchers))) {
+		LIST_REMOVE(curr, list);
+		free(curr->mask);
+		free(curr);
+	}
+}
+
+/* adds a watcher with a shell style mask
+ * func can return 0 to end the chain, or return 1 if the operation should continue on */
+EXPORT void config_watch(struct config *cfg, const char *mask, int (*func)(struct config *cfg, void *extra, const char *id, const char *value), void *extra) {
+	struct config_watcher *w;
+	assert(mask != NULL);
+	assert(cfg != NULL);
+	w=malloc(sizeof *w);
+	w->mask=strdup(mask);
+	w->func=func;
+	w->extra=extra;
+	LIST_INSERT_HEAD(&cfg->watchers, w, list);
+}
+
+EXPORT int config_load(const char *filename, struct config *cfg) {
+	char buf[1024];
+	FILE *f;
+	char *e, *value;
+	unsigned line;
+	char quote;
+	struct config_watcher *curr;
+
+	f=fopen(filename, "r");
+	if(!f) {
+		PERROR(filename);
+		return 0;
+	}
+	line=0;
+	while(line++,fgets(buf, sizeof buf, f)) {
+		/* strip comments - honors '' and "" quoting */
+		for(e=buf,quote=0;*e;e++) {
+			if(!quote && *e=='"')
+				quote=*e;
+			else if(!quote && *e=='\'')
+				quote=*e;
+			else if(quote=='\'' && *e=='\'')
+				quote=0;
+			else if(quote=='"' && *e=='"')
+				quote=0;
+			else if(!quote && ( *e=='#' || (*e=='/' && e[1]=='/' ))) {
+				*e=0; /* found a comment */
+				break;
+			}
+		}
+
+		/* strip trailing white space */
+		e=buf+strlen(buf);
+		while(e>buf && isspace(*--e)) {
+			*e=0;
+		}
+
+		/* ignore blank lines */
+		if(*buf==0) {
+			TRACE("%s:%d:ignoring blank line\n", filename, line);
+			continue;
+		}
+
+		e=strchr(buf, '=');
+		if(!e) {
+			/* invalid directive */
+			ERROR_FMT("%s:%d:invalid directive\n", filename, line);
+			goto failure;
+		}
+
+		/* move through the leading space of the value part */
+		value=e+1;
+		while(isspace(*value)) value++;
+
+		/* strip trailing white space from id part */
+		*e=0; /* null terminate the id part */
+		while(e>buf && isspace(*--e)) {
+			*e=0;
+		}
+
+		TODO("dequote the value part");
+
+		/* printf("id='%s' value='%s'\n", buf, value); */
+
+		/* check the masks */
+		for(curr=LIST_TOP(cfg->watchers);curr;curr=LIST_NEXT(curr, list)) {
+			if(!util_fnmatch(curr->mask, buf, UTIL_FNM_CASEFOLD) && curr->func) {
+				int res;
+				res=curr->func(cfg, curr->extra, buf, value);
+				if(!res) {
+					break; /* return 0 from the callback will terminate the list */
+				} else if(res<0) {
+					ERROR_FMT("%s:%u:error in loading file\n", filename, line);
+					goto failure;
+				}
+			}
+		}
+	}
+	fclose(f);
+	return 1; /* success */
+failure:
+	fclose(f);
+	return 0; /* failure */
+}
+
+#ifndef NTEST
+static int config_test_show(struct config *cfg UNUSED, void *extra UNUSED, const char *id, const char *value) {
+	printf("CONFIG SHOW: %s=%s\n", id, value);
+	return 1;
+}
+
+static void config_test(void) {
+	struct config cfg;
+	config_setup(&cfg);
+	config_watch(&cfg, "s*er.*", config_test_show, 0);
+	config_load("test.cfg", &cfg);
+	config_free(&cfg);
+}
+#endif
+
 
 /******************************************************************************
  * Bitmap API
@@ -4259,143 +4392,6 @@ EXPORT int game_init(void) {
 
 	return 1;
 }
-
-/******************************************************************************
- * Config loader
- ******************************************************************************/
-struct config_watcher {
-	LIST_ENTRY(struct config_watcher) list;
-	char *mask;
-	int (*func)(struct config *cfg, void *extra, const char *id, const char *value);
-	void *extra;
-};
-
-EXPORT void config_setup(struct config *cfg) {
-	LIST_INIT(&cfg->watchers);
-}
-
-EXPORT void config_free(struct config *cfg) {
-	struct config_watcher *curr;
-	assert(cfg != NULL);
-	while((curr=LIST_TOP(cfg->watchers))) {
-		LIST_REMOVE(curr, list);
-		free(curr->mask);
-		free(curr);
-	}
-}
-
-/* adds a watcher with a shell style mask
- * func can return 0 to end the chain, or return 1 if the operation should continue on */
-EXPORT void config_watch(struct config *cfg, const char *mask, int (*func)(struct config *cfg, void *extra, const char *id, const char *value), void *extra) {
-	struct config_watcher *w;
-	assert(mask != NULL);
-	assert(cfg != NULL);
-	w=malloc(sizeof *w);
-	w->mask=strdup(mask);
-	w->func=func;
-	w->extra=extra;
-	LIST_INSERT_HEAD(&cfg->watchers, w, list);
-}
-
-EXPORT int config_load(const char *filename, struct config *cfg) {
-	char buf[1024];
-	FILE *f;
-	char *e, *value;
-	unsigned line;
-	char quote;
-	struct config_watcher *curr;
-
-	f=fopen(filename, "r");
-	if(!f) {
-		PERROR(filename);
-		return 0;
-	}
-	line=0;
-	while(line++,fgets(buf, sizeof buf, f)) {
-		/* strip comments - honors '' and "" quoting */
-		for(e=buf,quote=0;*e;e++) {
-			if(!quote && *e=='"')
-				quote=*e;
-			else if(!quote && *e=='\'')
-				quote=*e;
-			else if(quote=='\'' && *e=='\'')
-				quote=0;
-			else if(quote=='"' && *e=='"')
-				quote=0;
-			else if(!quote && ( *e=='#' || (*e=='/' && e[1]=='/' ))) {
-				*e=0; /* found a comment */
-				break;
-			}
-		}
-
-		/* strip trailing white space */
-		e=buf+strlen(buf);
-		while(e>buf && isspace(*--e)) {
-			*e=0;
-		}
-
-		/* ignore blank lines */
-		if(*buf==0) {
-			TRACE("%s:%d:ignoring blank line\n", filename, line);
-			continue;
-		}
-
-		e=strchr(buf, '=');
-		if(!e) {
-			/* invalid directive */
-			ERROR_FMT("%s:%d:invalid directive\n", filename, line);
-			goto failure;
-		}
-
-		/* move through the leading space of the value part */
-		value=e+1;
-		while(isspace(*value)) value++;
-
-		/* strip trailing white space from id part */
-		*e=0; /* null terminate the id part */
-		while(e>buf && isspace(*--e)) {
-			*e=0;
-		}
-
-		TODO("dequote the value part");
-
-		/* printf("id='%s' value='%s'\n", buf, value); */
-
-		/* check the masks */
-		for(curr=LIST_TOP(cfg->watchers);curr;curr=LIST_NEXT(curr, list)) {
-			if(!util_fnmatch(curr->mask, buf, UTIL_FNM_CASEFOLD) && curr->func) {
-				int res;
-				res=curr->func(cfg, curr->extra, buf, value);
-				if(!res) {
-					break; /* return 0 from the callback will terminate the list */
-				} else if(res<0) {
-					ERROR_FMT("%s:%u:error in loading file\n", filename, line);
-					goto failure;
-				}
-			}
-		}
-	}
-	fclose(f);
-	return 1; /* success */
-failure:
-	fclose(f);
-	return 0; /* failure */
-}
-
-#ifndef NTEST
-static int config_test_show(struct config *cfg UNUSED, void *extra UNUSED, const char *id, const char *value) {
-	printf("CONFIG SHOW: %s=%s\n", id, value);
-	return 1;
-}
-
-static void config_test(void) {
-	struct config cfg;
-	config_setup(&cfg);
-	config_watch(&cfg, "s*er.*", config_test_show, 0);
-	config_load("test.cfg", &cfg);
-	config_free(&cfg);
-}
-#endif
 
 /******************************************************************************
  * Mud Config
