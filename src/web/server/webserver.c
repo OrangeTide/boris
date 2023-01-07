aa4ba7ef05a0b32f24a7920f7644f35d00a0259f
Keegan <chaosfalcon@gmail.com>
2023-01-07 14:58:29 -0700

    match style

--- src/web/server/webserver.c
+++ src/web/server/webserver.c
@@ -1,40 +1,27 @@
 #define LOG_SUBSYSTEM "webserver"
 #include <log.h>
 
-#include <webserver.h>
 #include <libwebsockets.h>
+#include <webserver.h>
 
-void webserver_log_emit(int level, const char *line)
+void
+webserver_log_emit(int level, const char *line)
 {
 	int emit_level = 0;
-	switch (level)
-	{
-	case LLL_ERR:
-		emit_level = LOG_LEVEL_ERROR;
-		break;
-	case LLL_WARN:
-		emit_level = LOG_LEVEL_WARN;
-		break;
-	case LLL_NOTICE:
-		emit_level = LOG_LEVEL_INFO;
-		break;
-	case LLL_INFO:
-		emit_level = LOG_LEVEL_INFO;
-		break;
-	case LLL_DEBUG:
-		emit_level = LOG_LEVEL_DEBUG;
-		break;
-	case LLL_USER:
-		emit_level = LOG_LEVEL_INFO;
-		break;
-	default:
-		emit_level = LOG_LEVEL_WARN;
-		break;
+	switch (level) {
+	case LLL_ERR: emit_level = LOG_LEVEL_ERROR; break;
+	case LLL_WARN: emit_level = LOG_LEVEL_WARN; break;
+	case LLL_NOTICE: emit_level = LOG_LEVEL_INFO; break;
+	case LLL_INFO: emit_level = LOG_LEVEL_INFO; break;
+	case LLL_DEBUG: emit_level = LOG_LEVEL_DEBUG; break;
+	case LLL_USER: emit_level = LOG_LEVEL_INFO; break;
+	default: emit_level = LOG_LEVEL_WARN; break;
 	}
 	log_logf(emit_level, LOG_SUBSYSTEM, "%s", line);
 };
 
-int webserver_init(int family, unsigned port)
+int
+webserver_init(int family, unsigned port)
 {
 	lws_set_log_level(LLL_USER | LLL_ERR | LLL_WARN | LLL_NOTICE, webserver_log_emit);
 
@@ -42,7 +29,8 @@ int webserver_init(int family, unsigned port)
 	return 1;
 };
 
-void webserver_shutdown(void)
+void
+webserver_shutdown(void)
 {
 	lwsl_user("shutting down\n");
 	return;
