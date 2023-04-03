0f58a8b5717d55f8fc361fe633ae65e077dcfbe8
kibels <chaosfalcon@gmail.com>
2023-04-02 19:14:43 -0600

    mongoose server with websocket echo

--- src/web/server/webserver.c
+++ src/web/server/webserver.c
@@ -2,6 +2,7 @@
 #include <log.h>
 
 #include <mongoose.h>
+#include <dyad.h>
 #include <webserver.h>
 #include <pthread.h>
 #include <signal.h>
@@ -13,14 +14,65 @@ static struct webserver_context web_context;
 static sig_atomic_t interrupted = 0;
 static pthread_t webserver_thread;
 
+static const char *web_root = "./bin/www";
+static struct mg_mgr webserver_mgr;
+
+void 
+webserver_test_callback(dyad_Event* ev)
+{
+	char buf[48] = {0};
+
+	snprintf(buf, 47, "Webserver Event: %s", ev->data);
+	LOG_INFO("%s", buf);
+}
+
+static void 
+webserver_handler(struct mg_connection *c, int ev, void *ev_data, void *fn_data) 
+{
+	dyad_Stream *upstream = web_context.webserver_upstream;
+
+	if (ev == MG_EV_WS_OPEN)
+	{
+		LOG_INFO("websocket client connected");
+		dyad_write(upstream, (const void *)"@NEW_CLIENT@", 13);
+	}
+	else if (ev == MG_EV_HTTP_MSG) 
+	{
+		struct mg_http_message *hm = (struct mg_http_message *) ev_data;
+		if (mg_http_match_uri(hm, "/ws")) 
+		{
+			// Upgrade to websocket. From now on, a connection is a full-duplex
+			// Websocket connection, which will receive MG_EV_WS_MSG events.
+			mg_ws_upgrade(c, hm, NULL);
+		} 
+		else if (mg_http_match_uri(hm, "/api")) 
+		{
+			// Serve REST response
+			mg_http_reply(c, 200, "", "{\"result\": \"%s\"}\n", "boris");
+    	} 
+		else 
+		{
+			// Serve static files
+			struct mg_http_serve_opts opts = {.root_dir = web_root};
+			mg_http_serve_dir(c, ev_data, &opts);
+    	}
+	} 
+	else if (ev == MG_EV_WS_MSG) 
+	{
+		// Got websocket frame. Received data is wm->data. Echo it back!
+		struct mg_ws_message *wm = (struct mg_ws_message *) ev_data;
+		mg_ws_send(c, wm->data.ptr, wm->data.len, WEBSOCKET_OP_TEXT);
+
+		dyad_write(upstream, (const void *)"@MESSAGE@", 9);
+	}
+	(void) fn_data;
+}
 
 void *
-webserver_service(void *arg){
-	int res = -1;
+webserver_service(void *arg)
+{
 	while (!interrupted) {
-		if (res < 0) {
-			break;
-		}
+		mg_mgr_poll(&webserver_mgr, 1000);
 	}
 	return NULL;
 }
@@ -28,7 +80,12 @@ webserver_service(void *arg){
 int
 webserver_init(struct webserver_context ctx, unsigned port)
 {
+	static char webserver_addr[32] = {0};
 	web_context = ctx;
+
+	mg_mgr_init(&webserver_mgr);
+	snprintf(webserver_addr, 31, "http://localhost:%d", port);
+	mg_http_listen(&webserver_mgr, webserver_addr, webserver_handler, NULL);
 
 	LOG_INFO("Starting webserver on port %d", port);
 
@@ -52,7 +109,7 @@ webserver_shutdown(void)
 	if (err) {
 		LOG_ERROR("failed to join webserver thread");
 	}
-
+	mg_mgr_free(&webserver_mgr);
 	LOG_INFO("webserver ended");
 	return;
 };
